87af2b95c922327f20257ca364fc0a6df5b5f449
AzimJaan Akhtamov <azimaxtamov2oo2@mail.ru>
2024-06-12 22:05:50 +0500

    test is finished. YandexAds is integrated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yandex_mobileads/mobile_ads.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Yandex ads'),
     );
   }
 }
@@ -31,46 +31,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
 
   @override
+  void initState() {
+    super.initState();
+    MobileAds.setUserConsent(true);
+    MobileAds.setAgeRestrictedUser(true);
+    MobileAds.initialize();
+  }
+BannerAdSize getAdSize(){
+final screenWidth= MediaQuery.sizeOf(context).width.round();
+return BannerAdSize.sticky(width: screenWidth);
+}
+
+  late BannerAd banner;
+  var isBannerAlreadyCreated = false;
+
+  _loadAd() async {
+    banner = _createBanner();
+    setState(() {
+      isBannerAlreadyCreated = true;
+    });
+    //banner.loadAd(adRequest: const AdRequest());
+  }
+
+_createBanner(){
+    return BannerAd(adUnitId: 'R-M-9298488-1', adSize: getAdSize(),
+    adRequest: const AdRequest(),
+    onAdLoaded: (){
+      if(!mounted){
+        banner.destroy();
+        return;
+      }
+    }
+    );
+}
+  @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
         title: Text(widget.title),
       ),
-      body: Center(
-       
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: isBannerAlreadyCreated? AdWidget(bannerAd: banner,) : null,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+        onPressed: _loadAd,
+        tooltip: 'Load Ad',
+        child: const Icon(Icons.image),
+      ),
     );
   }
 }
